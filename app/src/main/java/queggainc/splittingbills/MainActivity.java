4f5251ea10b75ad8a22a87cd4751ae2f14866c4b
Quegga <queggainc@gmail.com>
2018-07-06 21:19:02 +0200

    test3

--- app/src/main/java/queggainc/splittingbills/MainActivity.java
+++ app/src/main/java/queggainc/splittingbills/MainActivity.java
@@ -12,6 +12,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Button button = findViewById(R.id.button);
+        final Button button2 = findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                button2.setEnabled(true);
+                button.setEnabled(false);
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                button2.setEnabled(false);
+                button.setEnabled(true);
+            }
+        });
 
     }
 }
