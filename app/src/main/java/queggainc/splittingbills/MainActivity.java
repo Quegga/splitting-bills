56b0938234660e8e3e169f1eb63890f58c9192d3
Quegga <queggainc@gmail.com>
2018-07-06 20:46:40 +0200

    test1

---
+++ app/src/main/java/queggainc/splittingbills/MainActivity.java
@@ -0,0 +1,31 @@
+package queggainc.splittingbills;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final Button button = findViewById(R.id.button);
+        final Button button2 = findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (button2.isEnabled()) {
+                    button2.setEnabled(false);
+                } else {
+                    button2.setEnabled(true);
+                }
+            }
+        });
+
+
+    }
+}
